02e06e6cb64f9ba365de5c6699ccbe02ec08d8f0
Daniel Josh Uduma <116913814+Danieljosh-uduma@users.noreply.github.com>
2024-05-27 01:30:17 +0000

    create a post endpoint

--- handy/home/urls.py
+++ handy/home/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.posts , name='posts'),
-    path('posts/<str:pk>/', views.post_view, name='post_view')
+    path('posts/<str:pk>/', views.post_view, name='post-view'),
+    path('create-post/', views.create_post, name='create-post'),
+    path('update-post/<str:pk>/', views.update_post, name='update-post'),
+    path('delete-post/<str:pk>/', views.delete_post, name='delete-post')
 ]
--- handy/home/views.py
+++ handy/home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def posts(request):
@@ -8,6 +9,38 @@ def posts(request):
     return render(request, 'home/post.html', context)
 
 def post_view(request, pk):
-    post = Post.objects.get(head=pk)
+    post = Post.objects.get(id=pk)
     context = {'post': post}
     return render(request, 'home/post_view.html', context)
+
+def create_post(request):
+    form = PostForm
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts')
+    context = {'form': form}
+    return render(request, 'home/create_post.html', context)
+
+def update_post(request, pk):
+    post = Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts')
+        
+    context = {'form': form}
+    return render(request, 'home/create_post.html', context)
+
+def delete_post(request, pk):
+    post = Post.objects.get(id=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts')
+    context = {'obj': post}
+    return render(request, 'home/delete.html', context)
+    
